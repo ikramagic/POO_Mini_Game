1f75506f8c7364083661bb010001f355960a32ca
ikramiste <ikrame.saadi@gmail.com>
2023-10-17 13:06:23 +0200

    game works + did some layout for clarity

--- app.rb
+++ app.rb
@@ -9,8 +9,30 @@ require_relative 'lib/player'
 player1 = Player.new("Justine")
 player2 = Player.new("Gérard")
 
-puts "Voici l'état de nos deux joueurs :"
+puts "______>______>______| Bienvenue dans CamelFight Simulator |______<______<______"
+
+puts "\nVoici la jauge de CamelVie de nos deux joueurs :"
+    player1.show_state
+    player2.show_state
+
+    puts "\nPassons à la phase d'attaque :"
+
+while player1.life_points > 0 && player2.life_points > 0 do
+    player1.attacks(player2)
+
+if player2.life_points <= 0
+    break
+end
+
+player2.attacks(player1)
+if player1.life_points <= 0
+    break
+end
+
+end
+
+puts "________________________________________\n"
+
+puts "\nFin du match.\nScore final :"
 player1.show_state
 player2.show_state
-
-puts "Passons à la phase d'attaquer :"
