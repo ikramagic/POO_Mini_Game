98e6198e09ef8b7335f736cfdd56a312165b1950
ikramiste <ikrame.saadi@gmail.com>
2023-10-17 11:38:54 +0200

    execution of app started with state

--- app.rb
+++ app.rb
@@ -4,4 +4,13 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-binding.pry
+#binding.pry
+
+player1 = Player.new("Justine")
+player2 = Player.new("Gérard")
+
+puts "Voici l'état de nos deux joueurs :"
+player1.show_state
+player2.show_state
+
+puts "Passons à la phase d'attaquer :"
