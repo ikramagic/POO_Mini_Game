bc92e0d67290194e39e9770c07168f7f33570853
ikramiste <ikrame.saadi@gmail.com>
2023-10-17 12:51:55 +0200

    updated displayed text for better experience

--- lib/player.rb
+++ lib/player.rb
@@ -8,22 +8,21 @@ class Player
     end
 
     def show_state
-        puts "#{@name} a #{@life_points} points de vie." #affiche le life points correspondant au joueur selectionné
+        puts "#{@name} a #{@life_points} points de CamelVie." #affiche le life points correspondant au joueur selectionné
     end
 
     def gets_damage(damage)
         @life_points -= damage #sert à soustraire les dégats infligés
         if @life_points <= 0 #si la vie est à 0 alors...
-            return puts "Le joueur #{name} a été tué." #on arrête et on indique que le joueur est mort
+            return puts "RIP. \n#{name} a été tué." #on arrête et on indique que le joueur est mort
         end
     end
 
     def attacks(player_hit)
-        puts "Le joueur #{self.name} attaque le joueur #{player_hit.name} !" #self = l'instance qui appelle la méthode dans pry, player_hit la variable
+        puts "#{self.name} attrape #{player_hit.name}. \n#{self.name} lui assène un coup de babouche cosmique !" #self = l'instance qui appelle la méthode dans pry, player_hit la variable
         damage_inflicted = compute_damage #récupère un chiffre aléatoire fourni par méthode compute damage pr être utilisé ici
-        puts "Le joueur #{self.name} marave #{player_hit.name} et lui retire #{damage_inflicted} points !"
+        puts "#{self.name} marave son adversaire bien comme il faut. \n#{player_hit.name} a perdu #{damage_inflicted} points de vie suite à l'attaque de #{self.name}!"
         player_hit.gets_damage(damage_inflicted) #retire les points de vie avec methode gets damage en utilisant chiffre aléatoire fourni
-        puts player_hit.show_state #affiche la vie restante au joueur attaqué
     end
 
     def compute_damage #sert à générer un nombre aléatoire pour l'utiliser après dans l'attaque
